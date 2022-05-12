1ebd1aa4758e0ec7bb2456f41be151bbba340fae
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-05-12 22:26:10 +0700

    add kriteria

--- application/views/kriteria/tambah.php
+++ application/views/kriteria/tambah.php
@@ -1,100 +1,109 @@
-    <div class="page-header">
-    	<h1>Tambah Kriteria</h1>
-    </div>
-    <div class="col-lg-12">
-    	<div class="panel panel-info box-style-shadow">
-    		<div class="panel-body">
-    			<div class="errors">
-    				<?php
-					$errors = $this->session->flashdata('errors');
-					if (isset($errors)) {
-						foreach ($errors as $error) {
-					?>
-    						<div class="alert alert-danger alert-dismissable">
-    							<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-    							<?php echo $error; ?>
-    						</div>
-    				<?php
-						}
+<div class="card shadow mb-4">
+	<div class="card-header py-3 d-flex flex-md-row  justify-content-between">
+		<h6 class="m-0 font-weight-bold text-primary">Tambah Kriteria</h6>
+	</div>
+	<div class="card-body">
+		<form action="" class="form-horizontal" method="post" accept-charset="utf-8" id="mainForm">
+			<div class="errors">
+				<?php
+				$errors = $this->session->flashdata('errors');
+				if (isset($errors)) {
+					foreach ($errors as $error) {
+				?>
+						<div class="alert alert-danger alert-dismissable">
+							<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+							<?php echo $error; ?>
+						</div>
+				<?php
 					}
-					?>
-    			</div>
-    			<?php echo form_open('', array('class' => 'form-horizontal')) ?>
-    			<div class="form-group">
-    				<?php echo form_label('Kriteria :', '', array('for' => 'inputKriteria', 'class' => 'col-sm-2 control-label')) ?>
-    				<div class="col-sm-10">
-    					<?php echo form_input('kriteria', set_value('kriteria'), array('id' => 'inputKriteria', 'class' => 'form-control')) ?>
-    				</div>
-    			</div>
-    			<div class="form-group">
-    				<?php echo form_label('Sifat :', '', array('for' => 'inputSifat', 'class' => 'col-sm-2 control-label')) ?>
-    				<div class="col-sm-10">
-    					<label class="radio-inline">
-    						<?php echo form_radio('sifat', 'B', true) ?> Benefit
-    					</label>
-    					<label class="radio-inline">
-    						<?php echo form_radio('sifat', 'C') ?> Cost
-    					</label>
+				}
+				?>
+			</div>
 
-    				</div>
-    			</div>
-    			<div class="form-group">
-    				<?php echo form_label('Bobot :', '', array('for' => 'inputBobot', 'class' => 'col-sm-2 control-label')) ?>
-    				<div class="col-sm-10">
-    					<?php echo form_input('bobot', set_value('bobot'), array('id' => 'inputBobot', 'class' => 'form-control')) ?>
-    				</div>
-    			</div>
+			<div class="form-group row">
+				<label for="kriteria" class="col-sm-2 col-form-label">Kriteria</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="kriteria" name="kriteria" placeholder="Nama Kriteria">
+				</div>
+			</div>
 
-    			<div class="form-group">
-    				<?php echo form_label('Item Criteria :', '', array('class' => 'col-sm-2 control-label')) ?>
-    				<div class="col-sm-9">
-    					<?php echo form_input('itemKriteria1', set_value('itemKriteria1'), array('id' => 'itemKriteria1', 'class' => 'form-control')) ?>
-    				</div>
-    				<div class="col-sm-1">
-    					<p class="text-center">Value : 1</p>
-    				</div>
-    			</div>
-    			<div class="form-group">
-    				<div class=" col-sm-offset-2 col-sm-9">
-    					<?php echo form_input('itemKriteria2', set_value('itemKriteria2'), array('id' => 'itemKriteria2', 'class' => 'form-control')) ?>
-    				</div>
-    				<div class="col-sm-1">
-    					<p class="text-center">Value : 2</p>
-    				</div>
-    			</div>
-    			<div class="form-group">
-    				<div class=" col-sm-offset-2 col-sm-9">
-    					<?php echo form_input('itemKriteria3', set_value('itemKriteria3'), array('id' => 'itemKriteria3', 'class' => 'form-control')) ?>
-    				</div>
-    				<div class="col-sm-1">
-    					<p class="text-center">Value : 3</p>
-    				</div>
-    			</div>
-    			<div class="form-group">
-    				<div class=" col-sm-offset-2 col-sm-9">
-    					<?php echo form_input('itemKriteria4', set_value('itemKriteria4'), array('id' => 'itemKriteria4', 'class' => 'form-control')) ?>
-    				</div>
-    				<div class="col-sm-1">
-    					<p class="text-center">Value : 4</p>
-    				</div>
-    			</div>
-    			<div class="form-group">
-    				<div class=" col-sm-offset-2 col-sm-9">
-    					<?php echo form_input('itemKriteria5', set_value('itemKriteria5'), array('id' => 'itemKriteria5', 'class' => 'form-control')) ?>
-    				</div>
-    				<div class="col-sm-1">
-    					<p class="text-center">Value : 5</p>
-    				</div>
-    			</div>
+			<fieldset class="form-group">
+				<div class="row">
+					<legend class="col-form-label col-sm-2 pt-0">Sifat</legend>
+					<div class="col-sm-10">
+						<div class="form-check">
+							<input class="form-check-input" type="radio" name="sifat" id="benefit" value="B" checked>
+							<label class="form-check-label" for="benefit">
+								Benefit
+							</label>
+						</div>
+						<div class="form-check">
+							<input class="form-check-input" type="radio" name="sifat" id="cost" value="C">
+							<label class="form-check-label" for="cost">
+								Cost
+							</label>
+						</div>
+					</div>
+				</div>
+			</fieldset>
 
 
-    			<div class="form-group">
-    				<div class="col-sm-offset-2 col-sm-10">
-    					<?php echo form_submit('simpan', 'Simpan', array('class' => 'btn btn-success')) ?>
-    				</div>
-    			</div>
-    			<?php echo form_close() ?>
-    		</div>
-    	</div>
+			<div class="form-group row">
+				<label for="bobot" class="col-sm-2 col-form-label">Bobot</label>
+				<div class="col-sm-10">
+					<input type="number" class="form-control" id="bobot" name="bobot" placeholder="Jumlah Bobot">
+				</div>
+			</div>
 
-    </div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label">Item Kriteria</label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" name="itemKriteria1" placeholder="Nama Item Kriteria 1">
+				</div>
+				<div class="col-sm-2">
+					Value : <b>1</b>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" name="itemKriteria2" placeholder="Nama Item Kriteria 2">
+				</div>
+				<div class="col-sm-2">
+					Value : <b>2</b>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" name="itemKriteria3" placeholder="Nama Item Kriteria 3">
+				</div>
+				<div class="col-sm-2">
+					Value : <b>3</b>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" name="itemKriteria4" placeholder="Nama Item Kriteria 4">
+				</div>
+				<div class="col-sm-2">
+					Value : <b>4</b>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label"></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" name="itemKriteria5" placeholder="Nama Item Kriteria 5">
+				</div>
+				<div class="col-sm-2">
+					Value : <b>5</b>
+				</div>
+			</div>
+
+		</form>
+	</div>
+	<div class="card-footer">
+		<button class="btn btn-success" form="mainForm"><i class="fas fa-save"></i> Simpan</button>
+	</div>
+</div>
